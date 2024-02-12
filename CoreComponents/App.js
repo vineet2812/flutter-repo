96bc326edaad68c6b77f480c9045a08fabbe4474
unknown <vineetpashine@gmail.com>
2024-02-12 23:24:05 +0530

    commit:

--- CoreComponents/App.js
+++ CoreComponents/App.js
@@ -3,9 +3,8 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{flex:1,backgroundColor:'plum'}}>
-      <view style={{width:100,height:200, backgroundColor:'red'}}></view>
-      <view style={{width:100,height:200, backgroundColor:'green'}}></view>
+    <View style={{flex:1,backgroundColor:'plum', padding:60}}>
+      <Text style={{color:'white'}}>HelloWorld</Text>
     </View>
   );
 }

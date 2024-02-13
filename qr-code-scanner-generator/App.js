0ab93a9ad02220e6175041cb1ba92c1e7a1d55bf
vineet2812 <vineet_pashine@persistent.com>
2024-02-13 11:35:30 +0530

    imports

--- qr-code-scanner-generator/App.js
+++ qr-code-scanner-generator/App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput,Button,Alert } from 'react-native';
+import React,{useState} from 'react';
+import QRCode from 'react-native-qrcode-svg';
+import * as MediaLibrary from 'expo-media-library';
+import * as FileSystem from 'expo-file-system';
+import Toast from 'react-native-root-toast';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!asdas</Text>
+      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
